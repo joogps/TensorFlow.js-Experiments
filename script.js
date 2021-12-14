cca6047fc003f5805e3670160bfcdbf633340f33
João Gabriel Pozzobon dos Santos <joogps@gmail.com>
2021-12-14 14:27:57 -0300

    Adding new values

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
 model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 1]);
-const ys = tf.tensor2d([1, 4, 6, 8, 10, 12, 14, 16, 18, 20], [10, 1]);
+// y = 50+2x
+const xs = tf.tensor2d([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [16, 1]);
+const ys = tf.tensor2d([50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80], [16, 1]);
 
 async function train() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 750; i++) {
         const response = await model.fit(xs, ys);
         console.log(response.history.loss)
     }
 }
 
 train().then(() => {
-    model.predict(tf.tensor2d([42], [1, 1])).print()
-    model.save('downloads://my-model');
+    model.predict(tf.tensor2d([26, 27], [2, 1])).print()
+    //model.save('downloads://my-model');
 })
